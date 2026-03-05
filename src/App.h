270a1c453205a1a46cd448a21ca323609c52e90f
LVA0211 <lucasvanasbroeck@gmail.com>
2026-03-05 14:27:10 +0100

    Added documentation for most App methods Updated timesheet.xls

--- src/App.h
+++ src/App.h
@@ -15,19 +15,68 @@
 
 class App {
 public:
+    /**
+     * @brief Constructor of the App class.
+     */
     App();
+
     void parseFile(std::string filename);
     void writeToStream(std::ostream stream);
 
+    /**
+     * @brief Register a room.
+     *
+     * Registers a new 'Room' object.
+     * The App class expects ownership of the Room* passed in.
+     *
+     * @param room room to register
+     */
     void addRoom(Room* room);
+
+    /**
+     * @brief Retrieve a registered room based on its id.
+     *
+     * @param id id of the room to retrieve
+     * @return a pointer to the room with given id if it exists, nullptr otherwise
+     */
     Room* getRoom(const std::string& id);
 
+    /**
+     * @brief Register and plan a meeting.
+     *
+     * Register and plan a new meeting.
+     * The App expects ownership of the Meeting* passed in.
+     *
+     * @param meeting meeting to plan and register
+     */
     void addMeeting(Meeting* meeting);
+
+    /**
+     * @brief Retrieve a meeting based on its id.
+     *
+     * @param id id of the meeting to retrieve
+     * @return a pointer to the meeting with given id if it exists, nullptr otherwise
+     */
     Meeting* getMeeting(const std::string& id);
 
+    /**
+     * @brief Register a participation.
+     *
+     * Register a new participation.
+     * The App expects ownership of the Participation* passed in.
+     *
+     * @param participation participation to register
+     */
     void addParticipation(Participation* participation);
 
-    const std::list<Participation *> *getParticipationList(const std::string &id);
+    /**
+     * @brief Retrieve a list of all participations concerning the given user.
+     *
+     * @param user user for which to return all participations
+     * @return a const pointer to the requested list, if it exists;\n
+     *         nullptr otherwise (if the user has no registered participations)
+     */
+    const std::list<Participation *> *getParticipationList(const std::string &user);
 
     void processMeetings();
     ~App();
@@ -35,6 +84,7 @@ private:
     std::unordered_map<std::string, Room*> rooms;
     std::unordered_map<std::string, Meeting*> meetings;
     std::unordered_map<std::string, std::list<Participation*>> participation_lists;
+
 };
 
 
