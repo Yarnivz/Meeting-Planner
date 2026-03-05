2f61554cefbf039b114e5ec6e1d7646c3730eae6
LVA0211 <lucasvanasbroeck@gmail.com>
2026-03-05 15:41:46 +0100

    Design by contract

--- src/Meeting.cpp
+++ src/Meeting.cpp
@@ -4,9 +4,15 @@
 
 #include "Meeting.h"
 
+#include "DesignByContract.h"
+
 Meeting::Meeting(const std::string &label, const std::string &id, const std::string &room, const Date &date)
 : label(label), id(id), room(room), date(date)
 {
+    REQUIRE(!id.empty(), "Failed to construct meeting. 'id' can not be empty.");
+    REQUIRE(!id.empty(), "Failed to construct meeting. 'room' can not be empty.");
+    REQUIRE(date.isProperlyInitialized(), "Failed to construct meeting. 'date' has to be properly initialized with the constructor.");
+
     init_check_this_ptr = this;
 }
 
@@ -15,9 +21,15 @@ bool Meeting::isProperlyInitialized() const {
 }
 
 
-const std::string& Meeting::getId() { return id; }
+const std::string& Meeting::getId() {
+    REQUIRE(isProperlyInitialized(), "Failed to get id. Room has to be properly initialized with the constructor.");
+    return id;
+}
 
-const std::string& Meeting::toString() { return label; }
+const std::string& Meeting::toString() {
+    REQUIRE(isProperlyInitialized(), "Failed to convert to string. Room has to be properly initialized with the constructor.");
+    return label;
+}
 
 std::ostream & operator<<(std::ostream &os, Meeting &meeting) {
     os << meeting.toString();
