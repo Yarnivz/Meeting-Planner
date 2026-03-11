cb1a7fead5607a38152b4e80163289ae2d32bb0e
Yarnivz <yarnivanzeebroeck10@gmail.com>
2026-03-12 00:30:59 +0100

    added simple stream writer test

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
 
+#include "App.h"
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    App* app = new App();
+    Room* room = new Room("M.A.123", "a123", 10);
+    Meeting* m = new Meeting("First meeting", "123", "a123", Date(2026, 1, 31));
+    app->addRoom(room);
+    app->addMeeting(m);
+    app->writeToStream(std::cout);
+    delete app;
+    delete room;
+    delete m;
 }
