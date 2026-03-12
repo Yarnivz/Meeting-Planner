5e74bf201f31768d1aaf2cdefbb5d900bac9b408
Yarnivz <yarnivanzeebroeck10@gmail.com>
2026-03-12 22:43:48 +0100

    Added main code

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,12 @@
 #include <iostream>
 
 #include "App.h"
+#include <fstream>
 
 int main() {
     App* app = new App();
-    Room* room = new Room("M.A.123", "a123", 10);
-    Meeting* m = new Meeting("First meeting", "123", "a123", Date(2026, 1, 31));
-    app->addRoom(room);
-    app->addMeeting(m);
-    app->writeToStream(std::cout);
+    app->parseFile("./test-files/HappyDay1.xml");
+    std::ofstream fout("output.txt");
+    app->writeToStream(fout);
     delete app;
-    delete room;
-    delete m;
 }
