894af5d08e781155da9de322ef90e5291f1c27bd
tishaak <ishaak2800@gmail.com>
2026-03-15 14:55:08 +0100

    resolve merge issue

---
+++ src/Apptests.h
@@ -0,0 +1,16 @@
+//
+// Created by tishaak on 3/15/26.
+//
+
+#ifndef MEETING_PLANNER_APPTESTS_H
+#define MEETING_PLANNER_APPTESTS_H
+#include <gtest/gtest.h>
+#include "App.h"
+
+class AppTests : public ::testing::Test {
+protected:
+};
+
+
+
+#endif //MEETING_PLANNER_APPTESTS_H
