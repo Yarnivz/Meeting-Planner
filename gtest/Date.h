c6a3fad131d5abf3a419552541bce646c8463245
tishaak <ishaak2800@gmail.com>
2026-03-29 23:51:09 +0200

    Add ability to detect non-contract documentation and apropriately add placeholder contracts to ContractDocumentationGenerator (tool is WIP)

---
+++ gtest/Date.h
@@ -0,0 +1,147 @@
+//
+// Created by Yarni on 2/26/2026.
+//
+
+#ifndef MEETING_PLANNER_DATE_H
+#define MEETING_PLANNER_DATE_H
+#include <iostream>
+#include <chrono>
+
+class Date {
+public:
+    /**
+     * @brief Creates the date class.
+     * The date class has a year, month and day attribute.
+     * When the date class constructor gets called without parameters, the date that gets created will be the current date.
+     */
+    Date();
+
+    /**
+     * @brief Copies a Date object.
+     * It copies all important values but makes sure the 'properlyInitialized' test still passes.
+     * @param d The Date to be copied.
+     */
+    Date(const Date& d);
+
+    /**
+     * @brief Creates the date class.
+     * The date class has a year, month and day attribute.
+     * When the date class constructor gets called with parameters, all parameters are required and the date will be initialized with the given data.
+     * @param year
+     * @param month
+     * @param day
+     */
+    Date(int year, int month, int day);
+
+    /**
+     * @brief Creates the date class.
+     * The date class has a year, month and day attribute.
+     * When the date class constructor gets called with parameters, all parameters are required and the date will be initialized with the given data.
+     * @param year_month_day date to initialize the Date class with.
+     */
+    Date(std::chrono::year_month_day year_month_day);
+
+    /**
+     * @brief Checks whether this Participation was properly initialized by the constructor.
+     * @return bool indicating result
+     */
+    bool isProperlyInitialized() const;
+
+    /**
+     * @brief Year getter.
+     * @return the date's year
+     */
+    int getYear() const;
+
+    /**
+     * @brief Month getter.
+     * @return the date's month
+     */
+    int getMonth() const;
+
+    /**
+     * @brief Day getter.
+     * @return the date's day
+     */
+    int getDay() const;
+
+    /**
+     * @brief Weekday getter.
+     * @return the date's weekday
+     */
+    std::string getWeekDay() const;
+
+    /**
+     * @brief Converts the date class to a readable string format.
+     * @return the date represented as a string.
+     * The date's string format is "YYYY-MM-DD"
+     */
+    std::string toString() const;
+
+    /**
+     * @brief Standard stream operator using the toString function to append the date to the stream.
+     * @param os stream to write on
+     * @param date to write on the stream
+     * @return the stream
+     */
+    friend std::ostream& operator<<(std::ostream &os, const Date& date);
+
+    /**
+     * @brief Compares 2 dates with each other.
+     * Returns "True" if they are the same.
+     * Returns "False" if they are NOT the same
+     * @param other date to compare with
+     * @return whether the dates are the same
+     */
+    bool operator==(const Date &other) const;
+
+    /**
+     * @brief Compares 2 dates with each other.
+     * Returns "True" if the date before the operator is more in the past than the date after the operator.
+     * Returns "False" if the date before the operator is NOT more in the past than the date after the operator.
+     * @param other date to compare with
+     * @return whether the date is lesser than the other date
+     */
+    bool operator<(const Date &other) const;
+
+    /**
+     * @brief Compares 2 dates with each other.
+     * Returns "True" if the date before the operator is more recent than the date after the operator.
+     * Returns "False" if the date before the operator is NOT more recent than the date after the operator.
+     * @param other date to compare with
+     * @return whether the date is greater than the other date
+     */
+    bool operator>(const Date &other) const;
+
+    /**
+     * @brief Compares 2 dates with each other.
+     * Returns "True" if the date before the operator is more in the past or the same as the date after the operator.
+     * Returns "False" if the date before the operator is NOT more in the past or the same as the date after the operator.
+     * @param other date to compare with
+     * @return whether the date is lesser or equal to the other date
+     */
+    bool operator<=(const Date &other) const;
+
+    /**
+     * @brief Compares 2 dates with each other.
+     * Returns "True" if the date before the operator is more in recent or the same as the date after the operator.
+     * Returns "False" if the date before the operator is NOT more recent or the same as the date after the operator.
+     * @param other date to compare with
+     * @return whether the date is greater or equal to the other date
+     */
+    bool operator>=(const Date &other) const;
+
+    /**
+     * @brief Default destructor.
+     */
+    ~Date();
+private:
+    std::chrono::year year{};
+    std::chrono::month month{};
+    std::chrono::day day{};
+
+    void* init_test_this_ptr;
+};
+
+
+#endif //MEETING_PLANNER_DATE_H
--- tools/ContractsDocumentationGenerator.cpp
+++ tools/ContractsDocumentationGenerator.cpp
@@ -18,8 +18,12 @@ int main() {
             std::string baseFilename = entry.path().stem().string();
             if (std::filesystem::exists(sourceDirectory+baseFilename+".h"))
             {
-                ContractsDocumentationGenerator::generateContractsDocumentation(baseFilename, sourceDirectory);
-                std::cout << baseFilename << " has header file "<< std::endl;
+                //Temporarily use Date only for small scale test
+                if (baseFilename == "Date")
+                {
+                    ContractsDocumentationGenerator::generateContractsDocumentation(baseFilename, sourceDirectory);
+                    std::cout << baseFilename << " has header file "<< std::endl;
+                }
             }
         }
     }
@@ -46,7 +50,8 @@ void ContractsDocumentationGenerator::generateContractsDocumentation(const std::
 
     std::string currentFunction;
     std::string previousFunction;
-    for (size_t i=0; i < headerFileLines.size(); ++i)
+    std::vector<std::string> contracts;
+    for (size_t i = 0; i < headerFileLines.size(); ++i)
     {
         if ((headerFileLines[i].find(");") != std::string::npos ||
             headerFileLines[i].find(") const;") != std::string::npos) &&
@@ -56,8 +61,61 @@ void ContractsDocumentationGenerator::generateContractsDocumentation(const std::
             currentFunction = headerFileLines[i];
             currentFunction.pop_back();
 
+            //TODO add codeFile for loop to find contracts in current function
+            
+            contracts.push_back("test " + currentFunction);
+            contracts.push_back("test1");
+            contracts.push_back("test2");
+            size_t documentationLastLine = 0;
+            size_t contractDocumentationFirstLine = 0;
+            for (size_t j = i-1; j > 0; --j)
+            {
+                if (headerFileLines[j].find(previousFunction) != std::string::npos || j < 5 )
+                {
+                    if (documentationLastLine != 0 && contractDocumentationFirstLine != 0)
+                    {
+
+                    } else if (documentationLastLine != 0 && contractDocumentationFirstLine == 0)
+                    {
+                        std::cout << "adding fresh contract                          --------";
+                        headerFileLines.insert((headerFileLines.begin() + documentationLastLine), "     *");
+                        headerFileLines.insert((headerFileLines.begin() + documentationLastLine + 1), "     * @contracts");
+
+                        headerFileLines.insert((headerFileLines.begin() + documentationLastLine + 2), "     * " + contracts[0]);
+                        for (size_t c = 1; c < contracts.size(); ++c)
+                        {
+                            headerFileLines.insert((headerFileLines.begin() + documentationLastLine + c + 2), "     * @n " + contracts[c]);
+                        }
+                        i += contracts.size() + 2;
+                    } else
+                    {
+                        std::cout << "No documentation found for " << headerFileLines[i] << std::endl;
+                        break;
+                    }
+                } else if (headerFileLines[j].find("@contracts") != std::string::npos)
+                {
+                   contractDocumentationFirstLine = j;
+
+                } else if (headerFileLines[j].find("*/") != std::string::npos)
+                {
+                    documentationLastLine = j;
+                }
+            }
+            previousFunction = currentFunction;
             std::cout << " found function " << currentFunction << " in " << baseFilename+".h"<<std::endl;
+            contracts.clear();
         }
+
     }
+    codeFile.close();
+    headerFile.close();
+
+    //std::ofstream writableHeaderFile(sourceDirectory+baseFilename+".h"); //hold off until tested properly enough
+    for (std::string writeLine : headerFileLines)
+    {
+        std::cout << writeLine << std::endl;
+        //writableHeaderFile << writeLine << "\n"; //hold off until tested properly enough
+    }
+    //writableHeaderFile.close(); //hold off until tested properly enough
 
 }
