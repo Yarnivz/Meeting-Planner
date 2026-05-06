19082be8b6754fe225c04c534c65e4d489a8ebd8
wizardly_newton <@>
2026-05-06 20:22:42 +0200

    change main.cpp to use new system

--- src/main.cpp
+++ src/main.cpp
@@ -3,17 +3,19 @@
 #include "App.h"
 #include <fstream>
 
+#include "error/DetailedError.h"
+#include "error/ConsiseError.h"
 #include "output/StreamOutput.h"
 #include "parser/XmlParser.h"
 
 int main()
 {
 
-    XmlParser parser =  XmlParser("./test-files/Renovations.xml");
+    std::ofstream null_stream;
+    XmlParser parser =  XmlParser("./test-files/InvalidDataSystemErrors.xml");
 
     App* app = new App();
-    app->parseFile(parser);
-    app->processAllMeetings();
-    app->writeToStream(StreamOutput(std::cout));
+    app->parseFile(parser, ConsiseError(std::cerr));
+    //app->writeToStream(StreamOutput(std::cout));
     delete app;
 }
